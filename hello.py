db5363978600de02777fe9bbe7f6b1fae07a7612
lyly-vuongtu <tranthilyly30102003@gmail.com>
2023-12-09 15:45:21 +0700

    01hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,20 @@
-input("What's your name? ")
-print("hello, world")
+#Ask user for their name
+name = input("What's your name? ")
+#Remove whitespace from str
+name = name.strip()
+#Say hello to user
+"""
+print("hello,",name) # with ,
+print("Hello, "+name) # with +
+print("Hello, ", end="?")
+print(name)
+print("hello,", name, sep="123456")
+"""
+
+"""
+is a comment
+hello
+"""
+print(f"hello, {name}")
+#print('hello, "friend"')
+#print("hello, \"friend\"")
